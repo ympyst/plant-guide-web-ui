248b598a20948ef4abf05766fdc72f4f136a220d
Polina <ympyst@gmail.com>
2020-09-01 22:49:51 +0300

    test, readme and redundant code

--- src/App.test.js
+++ src/App.test.js
@@ -4,12 +4,12 @@ import { Provider } from 'react-redux';
 import store from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders header', () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/Определитель семейств/i)).toBeInTheDocument();
 });
